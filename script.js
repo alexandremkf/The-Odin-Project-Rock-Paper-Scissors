4aedf4e28a6bc61c900b7da3e974843a4008c0bc
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-30 15:35:38 -0300

    Fiz a lógica em javascript para que a maquina faça a própria escolha dela.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Oi")
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3)
+
+    if (randomNumber === 0) {
+        return "rock"
+    } else if (randomNumber === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
